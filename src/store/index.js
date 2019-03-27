4b99b6918412a196d59b227341f1c2cb30deff74
Willian Home <willian@wiki4fit.com.br>
2019-03-27 00:41:47 -0300

    adicionado reactotron

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+import { createStore, compose } from 'redux';
+
+import reducers from './reducers';
+
+const tronMiddleware = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer : () => {};
+const store = createStore(reducers, compose(tronMiddleware()));
+
+export default store;
